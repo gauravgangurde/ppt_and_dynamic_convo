3de04965a0184bcc7c9f188fa27fde88955c049e
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-06 15:55:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ with st.form("my_form"):
             response = pandas_ai(df, prompt=query)
             if isinstance(response, pd.DataFrame):
                 st.dataframe(response)
-                open('/prev_response.csv', 'wb').write(response.to_csv())
+                response.to_csv('prev_response.csv')
             else:
                 st.text(response.to_string(index=False))
 
