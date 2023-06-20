6d4dcb59d2a18b668f166a03c26f78eea0387aed
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-20 16:15:09 +0530

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -71,7 +71,7 @@ with tab3:
       response1 = openai_response(f"""Conversation Between AIG executive and customer
                   {convo1}
                   Based on the above conversation, provide what the executive should say next?. Provide in follwing format
-                  executive - <response>""")
+                  Claims Executive - <response>""")
       #x = f"""{x}\n\n{''.join(ln[t:i])}\n\n ---------\n{response1}\n---------"""
       st.markdown(f"""{''.join(ln[t:i])}\n\n ~~~~~~~~~~\n{response1}\n~~~~~~~~~~\n\n""")
       t = i+1
