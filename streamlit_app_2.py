b004cf0d5ef039b3333019f90217ed98b3dca822
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-20 16:23:49 +0530

    Update streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -73,12 +73,12 @@ with tab3:
                   Based on the above conversation, provide what the executive should say next?. Provide in follwing format
                   Claims Executive - <response>""")
       x = f"""{x}\n\n{''.join(ln[t:i])}\n\n ---------\n{response1}\n---------"""
-      #st.markdown(f"""{''.join(ln[t:i])}\n\n ~~~~~~~~~~\n{response1}\n~~~~~~~~~~\n\n""")
+      #st.markdown(f"""{''.join(ln[t:i])}\n\n---------\n{response1}\n---------\n\n""")
       t = i+1
     with open("compare.txt", 'w') as file:
       file.write(x)
   with open("compare.txt","r") as file:
     res = file.read()
-  st.markdown(res)
+  st.markdown(res, unsafe_allow_html=False)
     
     
