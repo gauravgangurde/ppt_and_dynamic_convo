f5a20684c9e263238e4cf01a1807147a8cb7df60
Gaurav Gangurde <gauravgangurde79@gmail.com>
2023-06-23 16:43:08 +0530

    Create streamlit_App3.py

---
+++ streamlit_App3.py
@@ -0,0 +1,32 @@
+import streamlit as st
+from PIL import Image
+import openai
+
+
+openai.api_key = st.secrets["chat_gpt_key"]
+
+def openai_response(query):
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages = [
+    {"role":"system", "content":"You are helpful assistant."},
+    {"role":"user","content": query}
+    ],
+    temperature = 0.6,
+    )
+  return response.choices[0]['message']['content']  
+
+
+
+
+with st.form("form"):
+
+  url = st.text_input("URL")
+  query = st.text_input("Query")
+  submitted = st.form_submit_button("Submit")
+  
+  if submitted:
+    response = openai_response(f"""Please analyse graph at this URL: {url} and {query})
+    st.image(url)
+    st.markdown(response)
+
